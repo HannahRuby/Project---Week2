699937324622e168ea4698c78ca9119d28180dfd
HANNAH <HUPEE2001@YAHOO.COM>
2024-03-31 23:20:52 +0100

    elements update

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log("Hello");
+
+const cookieBtn = document.getElementById("cookieBtn");
+const upgradeBtn = document.getElementById("upgradeBtn");
+const cookieSpan = document.getElementById("cookieSpan");
+const cpsSpan = document.getElementById("cpsSpan");
+
+const stats = {
+  cookieCount: 0,
+  cps: 0,
+};
+
+const storageStats = JSON.parse(localStorage.getItem("stats"));
+
+function buyCookie() {
+  stats.cookieCount++;
+  updatePage();
+  updateStorage();
+}
+
+function buyUpgrade() {
+  stats.cps++;
+  stats.cookieCount -= 10;
+  updatePage();
+  updateStorage();
+}
+cookieBtn.addEventListener("click", buyCookie);
+upgradeBtn.addEventListener("click", buyUpgrade);
+
+function updatePage() {
+  cpsSpan.textContent = stats.cps;
+  cookieSpan.textContent = stats.cookieCount;
+}
+function updateStorage() {
+  localStorage.setItem("stats", JSON.stringify(stats));
+}
+if (storageStats !== null) {
+  stats.cookieCount = storageStats.cookieCount;
+  stats.cps = storageStats.cps;
+  updatePage();
+}
+
+setInterval(function () {
+  stats.cookieCount += stats.cps;
+  updatePage();
+  updateStorage();
+}, 3000);
